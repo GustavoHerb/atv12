c171ca19af04f3b936fdfecbcc782b2981471e14
GustavoHerb <141774477+GustavoHerb@users.noreply.github.com>
2024-03-31 15:43:32 -0300

    Add files via upload

---
+++ script5.js
@@ -0,0 +1,9 @@
+function calculaAreaRetangulo(largura, altura) {
+    var area = largura * altura;
+    return area;
+}
+
+var larguraRetangulo = 5;
+var alturaRetangulo = 3;
+var areaRetangulo = calculaAreaRetangulo(larguraRetangulo, alturaRetangulo);
+console.log("A área do retângulo é: " + areaRetangulo);
